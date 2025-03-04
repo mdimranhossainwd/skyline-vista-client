4c33bbc703f49485d8a2dadc5dfa6394b342ff68
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-04 10:44:46 +0600

    styles(descrription): improved to text size resposive for all device

--- src/components/sections/Description.jsx
+++ src/components/sections/Description.jsx
@@ -3,8 +3,8 @@ import { Text } from "@radix-ui/themes";
 export const Description = () => {
   return (
     <>
-      <div className="container mx-auto text-center my-20">
-        <Text size="7" className="font-medium">
+      <div className="container mx-auto text-center my-16">
+        <Text className="font-medium text-base sm:text-lg md:text-xl lg:text-[26px] ">
           From the moment you step foot on our grounds, <br /> you'll be
           enveloped in a world of refined <br /> elegance and warm hospitality
         </Text>
