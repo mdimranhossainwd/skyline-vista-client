d5a93690d0245f1fbe606526a3b0e90169eadfc9
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-04 10:06:33 +0600

    chore(dependencies): install and configure Radix UI for accessible components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import { router } from "./routes/router";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </StrictMode>
 );
