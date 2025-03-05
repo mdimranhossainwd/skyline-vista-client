7fe2a16255bbcf0a9cab02f386fd48a888de452a
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-05 21:45:13 +0600

    styles(sign-up): Add password input field to the form

--- src/components/form/SignUp.jsx
+++ src/components/form/SignUp.jsx
@@ -6,55 +6,82 @@ export const SignUp = () => {
       {/* Left Side: Form Section */}
       <div className="flex items-center justify-center w-full lg:w-1/2 p-8">
         <div className="max-w-md w-full">
-          <h2 className="text-2xl font-semibold text-gray-900">
-            Sign in or create an account
-          </h2>
-          <p className="mt-2 text-gray-600">
-            Welcome back! Please enter your details.
-          </p>
+          <form>
+            <h2 className="text-2xl font-semibold text-gray-900">
+              Sign in or create an account
+            </h2>
+            <p className="mt-2 text-gray-600">
+              Welcome back! Please enter your details.
+            </p>
 
-          {/* Form */}
-          <div className="mt-6">
-            <label className="block text-sm font-medium text-gray-700">
-              Name
-            </label>
-            <input
-              type="text"
-              className="mt-1 block w-full px-4 py-2 border  focus:ring focus:ring-[#E9E9E9]"
-              style={{
-                borderColor: "#E9E9E9",
-              }}
-            />
-          </div>
+            {/* Form */}
+            <div className="mt-6">
+              <label className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                // {...register("name", { required: true })}
+                className="mt-1 block w-full px-4 py-2 border  focus:ring focus:ring-[#E9E9E9]"
+                style={{
+                  borderColor: "#E9E9E9",
+                }}
+              />
+            </div>
 
-          <div className="mt-4">
-            <label className="block text-sm font-medium text-gray-700">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="mt-1 block w-full px-4 py-2 border  focus:ring focus:ring-[#E9E9E9]"
-              style={{
-                borderColor: "#E9E9E9",
-              }}
-            />
-          </div>
-          <div className="mt-4">
-            <label className="block text-sm font-medium text-gray-700">
-              Photo URL
-            </label>
-            <input
-              type="photo"
-              className="mt-1 block w-full px-4 py-2 border focus:ring-[#E9E9E9]"
-              style={{
-                borderColor: "#E9E9E9",
-              }}
-            />
-          </div>
+            <div className="mt-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                // {...register("email", { required: true })}
+                className="mt-1 block w-full px-4 py-2 border  focus:ring focus:ring-[#E9E9E9]"
+                style={{
+                  borderColor: "#E9E9E9",
+                }}
+              />
+            </div>
 
-          <button className="w-full mt-6 px-4 py-2 text-white bg-gray-800  hover:bg-gray-700">
-            Continue
-          </button>
+            <div className="mt-6">
+              <label className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                // {...register("password", { required: true })}
+                className="mt-1 block w-full px-4 py-2 border  focus:ring focus:ring-[#E9E9E9]"
+                style={{
+                  borderColor: "#E9E9E9",
+                }}
+              />
+            </div>
+
+            <div className="mt-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Photo URL
+              </label>
+              <input
+                type="photo"
+                id="photo"
+                // {...register("photo", { required: true })}
+                className="mt-1 block w-full px-4 py-2 border focus:ring-[#E9E9E9]"
+                style={{
+                  borderColor: "#E9E9E9",
+                }}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full mt-6 px-4 py-2 text-white bg-gray-800  hover:bg-gray-700"
+            >
+              Continue
+            </button>
+          </form>
 
           <div className="mt-4 text-center text-gray-500">or</div>
 
