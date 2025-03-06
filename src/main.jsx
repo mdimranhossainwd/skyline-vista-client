eb0986716190a1eab54925327c5580efef1705eb
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-06 10:03:31 +0600

    chore(dependencies): Install and configure react-hot-toast for notifications

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import AuthProvider from "./provider/AuthProvider";
@@ -9,6 +10,7 @@ import { router } from "./routes/router";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Theme>
+      <Toaster />
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
