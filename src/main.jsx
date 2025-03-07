8ba3e97a09a570842f05095adb21bfdce7388331
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-07 09:10:53 +0600

    chore(dependencies): Install Tan-Stack Query and set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
@@ -7,13 +8,18 @@ import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import AuthProvider from "./provider/AuthProvider";
 import { router } from "./routes/router";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Theme>
-      <Toaster />
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </Theme>
   </StrictMode>
 );
