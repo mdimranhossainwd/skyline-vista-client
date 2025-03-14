9b39b52e6f83943a804f75cb67eb733b30b4d22e
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-14 15:14:34 +0600

    perf: Improve all user data retrieval using TanStack Query

--- src/pages/dashboard/AllUsersPage.jsx
+++ src/pages/dashboard/AllUsersPage.jsx
@@ -1,6 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import { Heading } from "../../components/shared/heading/Heading";
+import { useAxios } from "../../hooks/useAxios";
 
 export const AllUsersPage = () => {
+  const axios = useAxios();
+
+  //   Load all All-Properties Data
+  const getData = async () => {
+    const { data } = await axios.get("/all-users");
+    return data?.data;
+  };
+
+  //   Fetch All Data to TanStack Query
+  const { data: getAllUserData, refetch } = useQuery({
+    queryKey: ["getAllUserData"],
+    queryFn: getData,
+  });
+
+  console.log(getAllUserData);
+
   return (
     <>
       <Heading heading="All User's Managed" />
