e87b2d6604b20f7f15f290a88a24014aa8860ba1
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-14 15:02:21 +0600

    perf: Improve property data and review retrieval using TanStack Query

--- src/pages/dashboard/AllReviewsPage.jsx
+++ src/pages/dashboard/AllReviewsPage.jsx
@@ -1,6 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import { Heading } from "../../components/shared/heading/Heading";
+import { useAxios } from "../../hooks/useAxios";
 
 export const AllReviewsPage = () => {
+  const axios = useAxios();
+
+  //   Load all reviews Data
+  const getData = async () => {
+    const { data } = await axios.get("/get-reviews");
+    return data?.data;
+  };
+
+  //   Fetch All Data to TanStack Query
+  const { data: getReviewData, refetch } = useQuery({
+    queryKey: ["getReviewData"],
+    queryFn: getData,
+  });
+
+  console.log(getReviewData);
+
   return (
     <>
       <Heading heading="All Reviews Mangaged" />
--- src/pages/dashboard/ManagePropertiesPage.jsx
+++ src/pages/dashboard/ManagePropertiesPage.jsx
@@ -1,6 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import { Heading } from "../../components/shared/heading/Heading";
+import { useAxios } from "../../hooks/useAxios";
 
 export const ManagePropertiesPage = () => {
+  const axios = useAxios();
+
+  //   Load all All-Properties Data
+  const getData = async () => {
+    const { data } = await axios.get("/get-rooms");
+    return data?.data;
+  };
+
+  //   Fetch All Data to TanStack Query
+  const { data: getPropertiesData, refetch } = useQuery({
+    queryKey: ["getPropertiesData"],
+    queryFn: getData,
+  });
+
+  console.log(getPropertiesData);
+
   return (
     <>
       <Heading heading="All Properties Managed" />
