c69bca18f21a77321ab786e28f48aae99823ee87
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-17 08:30:22 +0600

    refactor(NotFoundPage): Improved to ui color changes for some tags

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ export const NotFoundPage = () => {
       <section className="bg-white">
         <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
           <div className="flex flex-col items-center max-w-sm mx-auto text-center">
-            <p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 ">
+            <p className="p-3 text-xl font-medium text-[#E21B60] rounded-full bg-blue-50 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -48,7 +48,7 @@ export const NotFoundPage = () => {
                 <span>Go back</span>
               </button>
 
-              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-[#E21B60] rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-[#E21B60]">
+              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-[#E21B60] rounded-lg shrink-0 sm:w-auto hover:bg-[#E21B60] dark:hover:bg-[#E21B60] dark:bg-[#E21B60]">
                 Take me home
               </button>
             </div>
