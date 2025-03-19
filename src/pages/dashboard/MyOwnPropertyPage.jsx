2a55f1ac04de71ae9c614db31859c78eae34f7c4
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-19 08:51:30 +0600

    perf: Improve agent his own properties data retrieval using TanStack Query

--- src/pages/dashboard/MyOwnPropertyPage.jsx
+++ src/pages/dashboard/MyOwnPropertyPage.jsx
@@ -1,6 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import { Heading } from "../../components/shared/heading/Heading";
+import { useAuth } from "../../hooks/useAuth";
+import { useAxios } from "../../hooks/useAxios";
 
 export const MyOwnPropertyPage = () => {
+  const axios = useAxios();
+  const { user } = useAuth();
+
+  //  Load all Agent-own-Properties Data
+  const getData = async () => {
+    const { data } = await axios.get(`/get-room-by-email?email=${user?.email}`);
+    return data?.data;
+  };
+
+  // Fetch All Data to TanStack Query
+  const { data: getAgentOwnProperty, refetch } = useQuery({
+    queryKey: ["getAgentOwnProperty"],
+    queryFn: getData,
+  });
+
+  console.log(getAgentOwnProperty);
+
   return (
     <>
       <Heading heading="Your Own Property Here." />
